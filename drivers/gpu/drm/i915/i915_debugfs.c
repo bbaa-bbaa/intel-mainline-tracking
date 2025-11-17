intel/mainline-tracking

drm/i915: SR-IOV Enabling and Support
SR-IOV Enabling and Support. Ported from 6.9 Kernel following
dii-client(6.6) SRIOV changes.

Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
Signed-off-by: Kooran Paul, Princy <princy.kooran.paul@intel.com>

drivers/gpu/drm/i915/i915_debugfs.c
@@ -83,6 +83,16 @@ static int i915_capabilities(struct seq_file *m, void *data)
 	return 0;
 }
 
+static int sriov_info_show(struct seq_file *m, void *data)
+{
+	struct drm_i915_private *i915 = node_to_i915(m->private);
+	struct drm_printer p = drm_seq_file_printer(m);
+
+	i915_sriov_print_info(i915, &p);
+
+	return 0;
+}
+
 static char get_tiling_flag(struct drm_i915_gem_object *obj)
 {
 	switch (i915_gem_object_get_tiling(obj)) {
@@ -594,6 +604,15 @@ i915_drop_caches_get(void *data, u64 *val)
 	return 0;
 }
 
+static bool has_permanent_wakeref(struct drm_i915_private *i915)
+{
+	/*
+	 * XXX: When we have VFs enabled, PF take an untracked wakeref, so
+	 * we can't determine properly whether the GT PM is idle.
+	 */
+	return IS_SRIOV_PF(i915) && (pci_num_vf(to_pci_dev(i915->drm.dev)) > 0);
+}
+
 static int
 gt_drop_caches(struct intel_gt *gt, u64 val)
 {
@@ -613,9 +632,12 @@ gt_drop_caches(struct intel_gt *gt, u64 val)
 	}
 
 	if (val & DROP_IDLE) {
-		ret = intel_gt_pm_wait_for_idle(gt);
-		if (ret)
-			return ret;
+		if (!has_permanent_wakeref(gt->i915)) {
+			ret = intel_gt_pm_wait_for_idle(gt);
+			if (ret)
+				return ret;
+		}
+
 	}
 
 	if (val & DROP_RESET_ACTIVE && intel_gt_terminally_wedged(gt))
@@ -719,6 +741,14 @@ static const struct drm_info_list i915_debugfs_list[] = {
 	{"i915_wa_registers", i915_wa_registers, 0},
 	{"i915_sseu_status", i915_sseu_status, 0},
 	{"i915_rps_boost_info", i915_rps_boost_info, 0},
+	{"i915_sriov_info", sriov_info_show, 0},
+};
+
+static const struct drm_info_list i915_vf_debugfs_list[] = {
+	{"i915_capabilities", i915_capabilities, 0},
+	{"i915_gem_objects", i915_gem_object_info, 0},
+	{"i915_engine_info", i915_engine_info, 0},
+	{"i915_sriov_info", sriov_info_show, 0},
 };
 
 static const struct i915_debugfs_files {
@@ -728,28 +758,50 @@ static const struct i915_debugfs_files {
 	{"i915_perf_noa_delay", &i915_perf_noa_delay_fops},
 	{"i915_wedged", &i915_wedged_fops},
 	{"i915_gem_drop_caches", &i915_drop_caches_fops},
+}, i915_vf_debugfs_files[] = {
+	{"i915_wedged", &i915_wedged_fops},
+	{"i915_gem_drop_caches", &i915_drop_caches_fops},
 };
 
 void i915_debugfs_register(struct drm_i915_private *dev_priv)
 {
 	struct drm_minor *minor = dev_priv->drm.primary;
+	const struct drm_info_list *debugfs_list;
+	const struct i915_debugfs_files *debugfs_files;
+	size_t debugfs_files_size;
+	size_t debugfs_list_size;
 	int i;
 
 	i915_debugfs_params(dev_priv);
 
 	debugfs_create_file("i915_forcewake_user", S_IRUSR, minor->debugfs_root,
 			    to_i915(minor->dev), &i915_forcewake_fops);
-	for (i = 0; i < ARRAY_SIZE(i915_debugfs_files); i++) {
-		debugfs_create_file(i915_debugfs_files[i].name,
+
+	if (IS_SRIOV_VF(dev_priv)) {
+		debugfs_files = i915_vf_debugfs_files;
+		debugfs_list = i915_vf_debugfs_list;
+
+		debugfs_files_size = ARRAY_SIZE(i915_vf_debugfs_files);
+		debugfs_list_size = ARRAY_SIZE(i915_vf_debugfs_list);
+	} else {
+		debugfs_files = i915_debugfs_files;
+		debugfs_list = i915_debugfs_list;
+
+		debugfs_files_size = ARRAY_SIZE(i915_debugfs_files);
+		debugfs_list_size = ARRAY_SIZE(i915_debugfs_list);
+	}
+
+	for (i = 0; i < debugfs_files_size; i++) {
+		debugfs_create_file(debugfs_files[i].name,
 				    S_IRUGO | S_IWUSR,
 				    minor->debugfs_root,
 				    to_i915(minor->dev),
-				    i915_debugfs_files[i].fops);
+				    debugfs_files[i].fops);
 	}
 
-	drm_debugfs_create_files(i915_debugfs_list,
-				 ARRAY_SIZE(i915_debugfs_list),
-				 minor->debugfs_root, minor);
+	drm_debugfs_create_files(debugfs_list,
+				debugfs_list_size,
+				minor->debugfs_root, minor);
 
 	i915_gpu_error_debugfs_register(dev_priv);
 }

drivers/gpu/drm/i915/i915_drv.h
@@ -57,7 +57,10 @@
 #include "i915_params.h"
 #include "i915_perf_types.h"
 #include "i915_scheduler.h"
+#include "i915_sriov.h"
+#include "i915_sriov_types.h"
 #include "i915_utils.h"
+#include "i915_virtualization.h"
 #include "intel_device_info.h"
 #include "intel_memory_region.h"
 #include "intel_runtime_pm.h"
@@ -196,6 +199,15 @@ struct drm_i915_private {
 	struct i915_params params;
 
 	const struct intel_device_info *__info; /* Use INTEL_INFO() to access. */
+
+	/* i915 virtualization mode, use IOV_MODE() to access */
+	enum i915_iov_mode __mode;
+#define IOV_MODE(i915) ({                              \
+	BUILD_BUG_ON(!I915_IOV_MODE_NONE);              \
+	GEM_BUG_ON(!(i915)->__mode);                    \
+	(i915)->__mode;                                 \
+})
+
 	struct intel_runtime_info __runtime; /* Use RUNTIME_INFO() to access. */
 	struct intel_driver_caps caps;
 
@@ -204,6 +216,7 @@ struct drm_i915_private {
 	struct intel_uncore uncore;
 	struct intel_uncore_mmio_debug mmio_debug;
 
+	struct i915_sriov sriov;
 	struct i915_virtual_gpu vgpu;
 
 	struct intel_gvt *gvt;
@@ -394,15 +407,15 @@ static inline struct intel_gt *to_gt(const struct drm_i915_private *i915)
 
 #define IP_VER(ver, rel)		((ver) << 8 | (rel))
 
-#define GRAPHICS_VER(i915)		(RUNTIME_INFO(i915)->graphics.ip.ver)
-#define GRAPHICS_VER_FULL(i915)		IP_VER(RUNTIME_INFO(i915)->graphics.ip.ver, \
-					       RUNTIME_INFO(i915)->graphics.ip.rel)
+#define GRAPHICS_VER(i915)	(assert_graphics_ip_ver_ready(i915), RUNTIME_INFO(i915)->graphics.ip.ver)
+#define GRAPHICS_VER_FULL(i915)	IP_VER(GRAPHICS_VER(i915), \
+				RUNTIME_INFO(i915)->graphics.ip.rel)
 #define IS_GRAPHICS_VER(i915, from, until) \
 	(GRAPHICS_VER(i915) >= (from) && GRAPHICS_VER(i915) <= (until))
 
-#define MEDIA_VER(i915)			(RUNTIME_INFO(i915)->media.ip.ver)
-#define MEDIA_VER_FULL(i915)		IP_VER(RUNTIME_INFO(i915)->media.ip.ver, \
-					       RUNTIME_INFO(i915)->media.ip.rel)
+#define MEDIA_VER(i915)		(assert_media_ip_ver_ready(i915), RUNTIME_INFO(i915)->media.ip.ver)
+#define MEDIA_VER_FULL(i915)	IP_VER(MEDIA_VER(i915), \
+				RUNTIME_INFO(i915)->media.ip.rel)
 #define IS_MEDIA_VER(i915, from, until) \
 	(MEDIA_VER(i915) >= (from) && MEDIA_VER(i915) <= (until))
 
@@ -724,10 +737,18 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
  */
 #define HAS_64K_PAGES(i915) (INTEL_INFO(i915)->has_64k_pages)
 
-#define HAS_REGION(i915, id) (INTEL_INFO(i915)->memory_regions & BIT(id))
-#define HAS_LMEM(i915) HAS_REGION(i915, INTEL_REGION_LMEM_0)
+#define HAS_REGION(i915, i)                                    \
+	(RUNTIME_INFO(i915)->memory_regions & (i) &               \
+	(IS_SRIOV_VF(i915) ? ~VF_RESTRICTED_REGIONS : ~0))
+#define HAS_LMEM(i915) HAS_REGION(i915, BIT(INTEL_REGION_LMEM_0))
 
 #define HAS_EXTRA_GT_LIST(i915)   (INTEL_INFO(i915)->extra_gt_list)
+
+#define HAS_SRIOV(dev_priv)	(INTEL_INFO(dev_priv)->has_sriov)
+
+#define HAS_MEMORY_IRQ(dev_priv) (INTEL_INFO(dev_priv)->has_memirq)
+
+#define HAS_MEMORY_IRQ_STATUS(dev_priv) (HAS_MEMORY_IRQ(dev_priv) && IS_SRIOV_VF(dev_priv))
 
 /*
  * Platform has the dedicated compression control state for each lmem surfaces

bbaa-bbaa/intel-mainline-tracking

Signed-off-by: Zawawi, Muhammad Zul Husni <muhammad.zul.husni.zawawi@intel.com>

drivers/gpu/drm/i915/i915_debugfs.c
@@ -77,6 +77,16 @@ static int i915_capabilities(struct seq_file *m, void *data)
 	return 0;
 }
 
+static int sriov_info_show(struct seq_file *m, void *data)
+{
+	struct drm_i915_private *i915 = node_to_i915(m->private);
+	struct drm_printer p = drm_seq_file_printer(m);
+
+	i915_sriov_print_info(i915, &p);
+
+	return 0;
+}
+
 static char get_tiling_flag(struct drm_i915_gem_object *obj)
 {
 	switch (i915_gem_object_get_tiling(obj)) {
@@ -585,6 +595,15 @@ i915_drop_caches_get(void *data, u64 *val)
 	return 0;
 }
 
+static bool has_permanent_wakeref(struct drm_i915_private *i915)
+{
+	/*
+	 * XXX: When we have VFs enabled, PF take an untracked wakeref, so
+	 * we can't determine properly whether the GT PM is idle.
+	 */
+	return IS_SRIOV_PF(i915) && (pci_num_vf(to_pci_dev(i915->drm.dev)) > 0);
+}
+
 static int
 gt_drop_caches(struct intel_gt *gt, u64 val)
 {
@@ -604,9 +623,12 @@ gt_drop_caches(struct intel_gt *gt, u64 val)
 	}
 
 	if (val & DROP_IDLE) {
-		ret = intel_gt_pm_wait_for_idle(gt);
-		if (ret)
-			return ret;
+		if (!has_permanent_wakeref(gt->i915)) {
+			ret = intel_gt_pm_wait_for_idle(gt);
+			if (ret)
+				return ret;
+		}
+
 	}
 
 	if (val & DROP_RESET_ACTIVE && intel_gt_terminally_wedged(gt))
@@ -710,6 +732,14 @@ static const struct drm_info_list i915_debugfs_list[] = {
 	{"i915_wa_registers", i915_wa_registers, 0},
 	{"i915_sseu_status", i915_sseu_status, 0},
 	{"i915_rps_boost_info", i915_rps_boost_info, 0},
+	{"i915_sriov_info", sriov_info_show, 0},
+};
+
+static const struct drm_info_list i915_vf_debugfs_list[] = {
+	{"i915_capabilities", i915_capabilities, 0},
+	{"i915_gem_objects", i915_gem_object_info, 0},
+	{"i915_engine_info", i915_engine_info, 0},
+	{"i915_sriov_info", sriov_info_show, 0},
 };
 
 static const struct i915_debugfs_files {
@@ -719,25 +749,46 @@ static const struct i915_debugfs_files {
 	{"i915_perf_noa_delay", &i915_perf_noa_delay_fops},
 	{"i915_wedged", &i915_wedged_fops},
 	{"i915_gem_drop_caches", &i915_drop_caches_fops},
+}, i915_vf_debugfs_files[] = {
+	{"i915_wedged", &i915_wedged_fops},
+	{"i915_gem_drop_caches", &i915_drop_caches_fops},
 };
 
 void i915_debugfs_register(struct drm_i915_private *i915)
 {
 	struct dentry *debugfs_root = i915->drm.debugfs_root;
+	const struct drm_info_list *debugfs_list;
+	const struct i915_debugfs_files *debugfs_files;
+	size_t debugfs_files_size;
+	size_t debugfs_list_size;
 	int i;
 
 	i915_debugfs_params(i915);
 
 	debugfs_create_file("i915_forcewake_user", S_IRUSR, debugfs_root,
 			    i915, &i915_forcewake_fops);
-	for (i = 0; i < ARRAY_SIZE(i915_debugfs_files); i++) {
-		debugfs_create_file(i915_debugfs_files[i].name, S_IRUGO | S_IWUSR,
+	if (IS_SRIOV_VF(i915)) {
+		debugfs_files = i915_vf_debugfs_files;
+		debugfs_list = i915_vf_debugfs_list;
+
+		debugfs_files_size = ARRAY_SIZE(i915_vf_debugfs_files);
+		debugfs_list_size = ARRAY_SIZE(i915_vf_debugfs_list);
+	} else {
+		debugfs_files = i915_debugfs_files;
+		debugfs_list = i915_debugfs_list;
+
+		debugfs_files_size = ARRAY_SIZE(i915_debugfs_files);
+		debugfs_list_size = ARRAY_SIZE(i915_debugfs_list);
+	}
+	
+	for (i = 0; i < debugfs_files_size; i++) {
+		debugfs_create_file(debugfs_files[i].name, S_IRUGO | S_IWUSR,
 				    debugfs_root, i915,
-				    i915_debugfs_files[i].fops);
+				    debugfs_files[i].fops);
 	}
 
-	drm_debugfs_create_files(i915_debugfs_list,
-				 ARRAY_SIZE(i915_debugfs_list),
+	drm_debugfs_create_files(debugfs_list,
+				 debugfs_list_size,
 				 debugfs_root, i915->drm.primary);
 
 	i915_gpu_error_debugfs_register(i915);

drivers/gpu/drm/i915/i915_drv.h
@@ -53,7 +53,10 @@
 #include "i915_params.h"
 #include "i915_perf_types.h"
 #include "i915_scheduler.h"
+#include "i915_sriov.h"
+#include "i915_sriov_types.h"
 #include "i915_utils.h"
+#include "i915_virtualization.h"
 #include "intel_device_info.h"
 #include "intel_memory_region.h"
 #include "intel_runtime_pm.h"
@@ -183,6 +186,15 @@ struct drm_i915_private {
 	struct i915_params params;
 
 	const struct intel_device_info *__info; /* Use INTEL_INFO() to access. */
+
+	/* i915 virtualization mode, use IOV_MODE() to access */
+	enum i915_iov_mode __mode;
+#define IOV_MODE(i915) ({                              \
+	BUILD_BUG_ON(!I915_IOV_MODE_NONE);              \
+	GEM_BUG_ON(!(i915)->__mode);                    \
+	(i915)->__mode;                                 \
+})
+
 	struct intel_runtime_info __runtime; /* Use RUNTIME_INFO() to access. */
 	struct intel_driver_caps caps;
 
@@ -191,6 +203,7 @@ struct drm_i915_private {
 	struct intel_uncore uncore;
 	struct intel_uncore_mmio_debug mmio_debug;
 
+	struct i915_sriov sriov;
 	struct i915_virtual_gpu vgpu;
 
 	struct intel_gvt *gvt;
@@ -360,15 +373,15 @@ static inline struct intel_gt *to_gt(const struct drm_i915_private *i915)
 
 #define IP_VER(ver, rel)		((ver) << 8 | (rel))
 
-#define GRAPHICS_VER(i915)		(RUNTIME_INFO(i915)->graphics.ip.ver)
-#define GRAPHICS_VER_FULL(i915)		IP_VER(RUNTIME_INFO(i915)->graphics.ip.ver, \
-					       RUNTIME_INFO(i915)->graphics.ip.rel)
+#define GRAPHICS_VER(i915)	(assert_graphics_ip_ver_ready(i915), RUNTIME_INFO(i915)->graphics.ip.ver)
+#define GRAPHICS_VER_FULL(i915)	IP_VER(GRAPHICS_VER(i915), \
+				RUNTIME_INFO(i915)->graphics.ip.rel)
 #define IS_GRAPHICS_VER(i915, from, until) \
 	(GRAPHICS_VER(i915) >= (from) && GRAPHICS_VER(i915) <= (until))
 
-#define MEDIA_VER(i915)			(RUNTIME_INFO(i915)->media.ip.ver)
-#define MEDIA_VER_FULL(i915)		IP_VER(RUNTIME_INFO(i915)->media.ip.ver, \
-					       RUNTIME_INFO(i915)->media.ip.rel)
+#define MEDIA_VER(i915)		(assert_media_ip_ver_ready(i915), RUNTIME_INFO(i915)->media.ip.ver)
+#define MEDIA_VER_FULL(i915)	IP_VER(MEDIA_VER(i915), \
+				RUNTIME_INFO(i915)->media.ip.rel)
 #define IS_MEDIA_VER(i915, from, until) \
 	(MEDIA_VER(i915) >= (from) && MEDIA_VER(i915) <= (until))
 
@@ -640,10 +653,18 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
  */
 #define HAS_64K_PAGES(i915) (INTEL_INFO(i915)->has_64k_pages)
 
-#define HAS_REGION(i915, id) (INTEL_INFO(i915)->memory_regions & BIT(id))
-#define HAS_LMEM(i915) HAS_REGION(i915, INTEL_REGION_LMEM_0)
+#define HAS_REGION(i915, i)                                    \
+	(INTEL_INFO(i915)->memory_regions & (i) &               \
+	(IS_SRIOV_VF(i915) ? ~VF_RESTRICTED_REGIONS : ~0))
+#define HAS_LMEM(i915) HAS_REGION(i915, BIT(INTEL_REGION_LMEM_0))
 
 #define HAS_EXTRA_GT_LIST(i915)   (INTEL_INFO(i915)->extra_gt_list)
+
+#define HAS_SRIOV(dev_priv)	(INTEL_INFO(dev_priv)->has_sriov)
+
+#define HAS_MEMORY_IRQ(dev_priv) (INTEL_INFO(dev_priv)->has_memirq)
+
+#define HAS_MEMORY_IRQ_STATUS(dev_priv) (HAS_MEMORY_IRQ(dev_priv) && IS_SRIOV_VF(dev_priv))
 
 /*
  * Platform has the dedicated compression control state for each lmem surfaces